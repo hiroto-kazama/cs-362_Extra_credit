542988bf365ee6f393995be38c5923f951e4da8a
hiroto <kazamah@oregonstate.edu>
2021-06-06 23:47:21 -0700

    Update test.py

--- test.py
+++ test.py
@@ -5,8 +5,8 @@ import reverse
 class TestCase(unittest.TestCase):
 
     def test_reverse(self):
-        Var = reverse.reverse("test is done")
-        self.assertEqual(Var, "enod si tset")
+        Var = reverse.reverse("test")
+        self.assertEqual(Var, "tset")
 
 
 if __name__ == '__main__':
